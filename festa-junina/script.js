1a2ab868a26477ba6e7bac2454e18d303f834fdf
limitrofe <limitrofe@gmail.com>
2024-07-04 00:58:13 -0300

    atualizar festa junina receitas

--- festa-junina/script.js
+++ festa-junina/script.js
@@ -1,18 +1,17 @@
-// script.js
 const festaData = {
     10: {
         "Cachorro quente": 2,
         "Dadinho de tapioca": 2,
-        "Sanduíche de farofa de carne seca": null,
+        "Sanduba de carne seca": null,
         "Cuscuz paulista": null,
         "Saquinho de pipoca": 20,
-        "Amendoim torrado (em gr)": 300,
+        "Amendoim torrado em gr": 300,
         "Espetinhos": null,
         "Espiga de milho": null,
         "Bolo de fubá (pedaço)": 1,
         "Bolo flocão cremoso": 2,
         "Brigadeiro de paçoca (unidade)": 2,
-        "Caixa paçoquinha rolha": 1,
+        "Paçoquinha": 1,
         "Canjica com coco (12 unidades)": 1.5,
         "Sagu com vinho (20 unidades)": null,
         "Quentão (3 lts)": 0.5,
@@ -22,25 +21,37 @@ const festaData = {
         "Latas suco": 2,
         "Latas água": 3,
         "recipeLink": {
-            "Cachorro quente": "https://example.com/receita-cachorro-quente",
-            "Dadinho de tapioca": "https://example.com/receita-dadinho-tapioca",
-            "Saquinho de pipoca": "https://example.com/receita-pipoca",
-            // Adicione os links das receitas para todos os itens
+            "Cachorro quente": "https://receitas.globo.com/series-originais/ju-ferraz-no-receitas/cachorro-quente-de-forno-gnt.ghtml",
+            "Dadinho de tapioca": "https://receitas.globo.com/tipos-de-prato/paes-e-salgados/dadinhos-de-tapioca-com-molho-agridoce-gnt.ghtml",
+            "Saquinho de pipoca": "https://receitas.globo.com/tipos-de-prato/paes-e-salgados/queijo-frito-para-pipoca-510a81054d3885190e000043.ghtml",
+            "Sanduba de carne seca": "https://receitas.globo.com/receitas-da-tv/perto-do-fogo/sanduiche-de-carne-seca-no-pao-de-abobora-gnt.ghtml",
+            "Cuscuz paulista": "https://receitas.globo.com/tipos-de-prato/aves/cuscuz-de-frango-cuscuz-paulista-506c6be4c5a6453aef000043.ghtml",
+            "Amendoim torrado em gr": "https://receitas.globo.com/tipos-de-prato/acompanhamentos/amendoim-torrado-salgado-503fa3404d38856870000042.ghtml",
+            "Espetinhos": "https://receitas.globo.com/blog/cardapios/espetinhos-confira-5-receitas-faceis-para-voce-testar-em-casa-g.ghtml",
+            "Espiga de milho": "https://receitas.globo.com/receitas-da-tv/bbb/edicao-2023/milho-cozido-simples.ghtml",
+            "Bolo de fubá (pedaço)": "https://receitas.globo.com/tipos-de-prato/bolos/bolo-de-fuba-cremoso-533302fe4d3885475f00003f.ghtml",
+            "Bolo flocão cremoso": "https://receitas.globo.com/tipos-de-prato/bolos/bolo-de-flocao-5424948b4d38855aab00008e.ghtml",
+            "Brigadeiro de paçoca (unidade)": "https://receitas.globo.com/regionais/eptv/brigadeiro-de-pacoca.ghtml",
+            "Paçoquinha": "https://receitas.globo.com/tipos-de-prato/doces-e-sobremesas/pacoquinha-51cc3e6a4d388562c0000042.ghtml",
+            "Canjica com coco (12 unidades)": "https://receitas.globo.com/tipos-de-prato/doces-e-sobremesas/canjica-cremosa-com-coco-51a397da4d38854665000041.ghtml",
+            "Sagu com vinho (20 unidades)": "https://receitas.globo.com/tipos-de-prato/doces-e-sobremesas/sagu-com-vinho-tinto-4d544b2d52e0b20f76001631.ghtml",
+            "Quentão (3 lts)": "https://receitas.globo.com/receitas-da-tv/mais-voce/quentao-4d50796652e0b252bc00176a.ghtml",
+            "Vinho quente (20 unidades)": "https://receitas.globo.com/datas-comemorativas/festa-junina/vinho-quente-5583143c4d38853625000045.ghtml",
         }
     },
     20: {
         "Cachorro quente": 2,
         "Dadinho de tapioca": 3,
-        "Sanduíche de farofa de carne seca": 4,
+        "Sanduba de carne seca": 4,
         "Cuscuz paulista": null,
         "Saquinho de pipoca": 40,
-        "Amendoim torrado (em gr)": 600,
+        "Amendoim torrado em gr": 600,
         "Espetinhos": null,
         "Espiga de milho": null,
         "Bolo de fubá (pedaço)": 1,
         "Bolo flocão cremoso": 1,
         "Brigadeiro de paçoca (unidade)": 1,
-        "Caixa paçoquinha rolha": 1,
+        "Paçoquinha": 1,
         "Canjica com coco (12 unidades)": 2,
         "Sagu com vinho (20 unidades)": null,
         "Quentão (3 lts)": 1,
@@ -50,25 +61,37 @@ const festaData = {
         "Latas suco": 4,
         "Latas água": 6,
         "recipeLink": {
-            "Cachorro quente": "https://example.com/receita-cachorro-quente",
-            "Dadinho de tapioca": "https://example.com/receita-dadinho-tapioca",
-            "Saquinho de pipoca": "https://example.com/receita-pipoca",
-            // Adicione os links das receitas para todos os itens
+            "Cachorro quente": "https://receitas.globo.com/series-originais/ju-ferraz-no-receitas/cachorro-quente-de-forno-gnt.ghtml",
+            "Dadinho de tapioca": "https://receitas.globo.com/tipos-de-prato/paes-e-salgados/dadinhos-de-tapioca-com-molho-agridoce-gnt.ghtml",
+            "Saquinho de pipoca": "https://receitas.globo.com/tipos-de-prato/paes-e-salgados/queijo-frito-para-pipoca-510a81054d3885190e000043.ghtml",
+            "Sanduba de carne seca": "https://receitas.globo.com/receitas-da-tv/perto-do-fogo/sanduiche-de-carne-seca-no-pao-de-abobora-gnt.ghtml",
+            "Cuscuz paulista": "https://receitas.globo.com/tipos-de-prato/aves/cuscuz-de-frango-cuscuz-paulista-506c6be4c5a6453aef000043.ghtml",
+            "Amendoim torrado em gr": "https://receitas.globo.com/tipos-de-prato/acompanhamentos/amendoim-torrado-salgado-503fa3404d38856870000042.ghtml",
+            "Espetinhos": "https://receitas.globo.com/blog/cardapios/espetinhos-confira-5-receitas-faceis-para-voce-testar-em-casa-g.ghtml",
+            "Espiga de milho": "https://receitas.globo.com/receitas-da-tv/bbb/edicao-2023/milho-cozido-simples.ghtml",
+            "Bolo de fubá (pedaço)": "https://receitas.globo.com/tipos-de-prato/bolos/bolo-de-fuba-cremoso-533302fe4d3885475f00003f.ghtml",
+            "Bolo flocão cremoso": "https://receitas.globo.com/tipos-de-prato/bolos/bolo-de-flocao-5424948b4d38855aab00008e.ghtml",
+            "Brigadeiro de paçoca (unidade)": "https://receitas.globo.com/regionais/eptv/brigadeiro-de-pacoca.ghtml",
+            "Paçoquinha": "https://receitas.globo.com/tipos-de-prato/doces-e-sobremesas/pacoquinha-51cc3e6a4d388562c0000042.ghtml",
+            "Canjica com coco (12 unidades)": "https://receitas.globo.com/tipos-de-prato/doces-e-sobremesas/canjica-cremosa-com-coco-51a397da4d38854665000041.ghtml",
+            "Sagu com vinho (20 unidades)": "https://receitas.globo.com/tipos-de-prato/doces-e-sobremesas/sagu-com-vinho-tinto-4d544b2d52e0b20f76001631.ghtml",
+            "Quentão (3 lts)": "https://receitas.globo.com/receitas-da-tv/mais-voce/quentao-4d50796652e0b252bc00176a.ghtml",
+            "Vinho quente (20 unidades)": "https://receitas.globo.com/datas-comemorativas/festa-junina/vinho-quente-5583143c4d38853625000045.ghtml",
         }
     },
     30: {
         "Cachorro quente": 3,
         "Dadinho de tapioca": 3,
-        "Sanduíche de farofa de carne seca": 10,
+        "Sanduba de carne seca": 10,
         "Cuscuz paulista": 30,
         "Saquinho de pipoca": 60,
-        "Amendoim torrado (em gr)": 900,
+        "Amendoim torrado em gr": 900,
         "Espetinhos": 30,
         "Espiga de milho": null,
         "Bolo de fubá (pedaço)": 2,
         "Bolo flocão cremoso": 1,
         "Brigadeiro de paçoca (unidade)": 1.5,
-        "Caixa paçoquinha rolha": 2,
+        "Paçoquinha": 2,
         "Canjica com coco (12 unidades)": 3,
         "Sagu com vinho (20 unidades)": 2,
         "Quentão (3 lts)": 1.5,
@@ -78,25 +101,37 @@ const festaData = {
         "Latas suco": 5,
         "Latas água": 6,
         "recipeLink": {
-            "Cachorro quente": "https://example.com/receita-cachorro-quente",
-            "Dadinho de tapioca": "https://example.com/receita-dadinho-tapioca",
-            "Saquinho de pipoca": "https://example.com/receita-pipoca",
-            // Adicione os links das receitas para todos os itens
+            "Cachorro quente": "https://receitas.globo.com/series-originais/ju-ferraz-no-receitas/cachorro-quente-de-forno-gnt.ghtml",
+            "Dadinho de tapioca": "https://receitas.globo.com/tipos-de-prato/paes-e-salgados/dadinhos-de-tapioca-com-molho-agridoce-gnt.ghtml",
+            "Saquinho de pipoca": "https://receitas.globo.com/tipos-de-prato/paes-e-salgados/queijo-frito-para-pipoca-510a81054d3885190e000043.ghtml",
+            "Sanduba de carne seca": "https://receitas.globo.com/receitas-da-tv/perto-do-fogo/sanduiche-de-carne-seca-no-pao-de-abobora-gnt.ghtml",
+            "Cuscuz paulista": "https://receitas.globo.com/tipos-de-prato/aves/cuscuz-de-frango-cuscuz-paulista-506c6be4c5a6453aef000043.ghtml",
+            "Amendoim torrado em gr": "https://receitas.globo.com/tipos-de-prato/acompanhamentos/amendoim-torrado-salgado-503fa3404d38856870000042.ghtml",
+            "Espetinhos": "https://receitas.globo.com/blog/cardapios/espetinhos-confira-5-receitas-faceis-para-voce-testar-em-casa-g.ghtml",
+            "Espiga de milho": "https://receitas.globo.com/receitas-da-tv/bbb/edicao-2023/milho-cozido-simples.ghtml",
+            "Bolo de fubá (pedaço)": "https://receitas.globo.com/tipos-de-prato/bolos/bolo-de-fuba-cremoso-533302fe4d3885475f00003f.ghtml",
+            "Bolo flocão cremoso": "https://receitas.globo.com/tipos-de-prato/bolos/bolo-de-flocao-5424948b4d38855aab00008e.ghtml",
+            "Brigadeiro de paçoca (unidade)": "https://receitas.globo.com/regionais/eptv/brigadeiro-de-pacoca.ghtml",
+            "Paçoquinha": "https://receitas.globo.com/tipos-de-prato/doces-e-sobremesas/pacoquinha-51cc3e6a4d388562c0000042.ghtml",
+            "Canjica com coco (12 unidades)": "https://receitas.globo.com/tipos-de-prato/doces-e-sobremesas/canjica-cremosa-com-coco-51a397da4d38854665000041.ghtml",
+            "Sagu com vinho (20 unidades)": "https://receitas.globo.com/tipos-de-prato/doces-e-sobremesas/sagu-com-vinho-tinto-4d544b2d52e0b20f76001631.ghtml",
+            "Quentão (3 lts)": "https://receitas.globo.com/receitas-da-tv/mais-voce/quentao-4d50796652e0b252bc00176a.ghtml",
+            "Vinho quente (20 unidades)": "https://receitas.globo.com/datas-comemorativas/festa-junina/vinho-quente-5583143c4d38853625000045.ghtml",
         }
     },
     40: {
         "Cachorro quente": 4,
         "Dadinho de tapioca": 4,
-        "Sanduíche de farofa de carne seca": 14,
+        "Sanduba de carne seca": 14,
         "Cuscuz paulista": 4,
         "Saquinho de pipoca": 80,
-        "Amendoim torrado (em gr)": 1200,
+        "Amendoim torrado em gr": 1200,
         "Espetinhos": 80,
         "Espiga de milho": 15,
         "Bolo de fubá (pedaço)": 2,
         "Bolo flocão cremoso": 2,
         "Brigadeiro de paçoca (unidade)": 2,
-        "Caixa paçoquinha rolha": 3,
+        "Paçoquinha": 3,
         "Canjica com coco (12 unidades)": 4,
         "Sagu com vinho (20 unidades)": 3,
         "Quentão (3 lts)": 2,
@@ -106,30 +141,83 @@ const festaData = {
         "Latas suco": 7,
         "Latas água": 10,
         "recipeLink": {
-            "Cachorro quente": "https://example.com/receita-cachorro-quente",
-            "Dadinho de tapioca": "https://example.com/receita-dadinho-tapioca",
-            "Saquinho de pipoca": "https://example.com/receita-pipoca",
-            // Adicione os links das receitas para todos os itens
+            "Cachorro quente": "https://receitas.globo.com/series-originais/ju-ferraz-no-receitas/cachorro-quente-de-forno-gnt.ghtml",
+            "Dadinho de tapioca": "https://receitas.globo.com/tipos-de-prato/paes-e-salgados/dadinhos-de-tapioca-com-molho-agridoce-gnt.ghtml",
+            "Saquinho de pipoca": "https://receitas.globo.com/tipos-de-prato/paes-e-salgados/queijo-frito-para-pipoca-510a81054d3885190e000043.ghtml",
+            "Sanduba de carne seca": "https://receitas.globo.com/receitas-da-tv/perto-do-fogo/sanduiche-de-carne-seca-no-pao-de-abobora-gnt.ghtml",
+            "Cuscuz paulista": "https://receitas.globo.com/tipos-de-prato/aves/cuscuz-de-frango-cuscuz-paulista-506c6be4c5a6453aef000043.ghtml",
+            "Amendoim torrado em gr": "https://receitas.globo.com/tipos-de-prato/acompanhamentos/amendoim-torrado-salgado-503fa3404d38856870000042.ghtml",
+            "Espetinhos": "https://receitas.globo.com/blog/cardapios/espetinhos-confira-5-receitas-faceis-para-voce-testar-em-casa-g.ghtml",
+            "Espiga de milho": "https://receitas.globo.com/receitas-da-tv/bbb/edicao-2023/milho-cozido-simples.ghtml",
+            "Bolo de fubá (pedaço)": "https://receitas.globo.com/tipos-de-prato/bolos/bolo-de-fuba-cremoso-533302fe4d3885475f00003f.ghtml",
+            "Bolo flocão cremoso": "https://receitas.globo.com/tipos-de-prato/bolos/bolo-de-flocao-5424948b4d38855aab00008e.ghtml",
+            "Brigadeiro de paçoca (unidade)": "https://receitas.globo.com/regionais/eptv/brigadeiro-de-pacoca.ghtml",
+            "Paçoquinha": "https://receitas.globo.com/tipos-de-prato/doces-e-sobremesas/pacoquinha-51cc3e6a4d388562c0000042.ghtml",
+            "Canjica com coco (12 unidades)": "https://receitas.globo.com/tipos-de-prato/doces-e-sobremesas/canjica-cremosa-com-coco-51a397da4d38854665000041.ghtml",
+            "Sagu com vinho (20 unidades)": "https://receitas.globo.com/tipos-de-prato/doces-e-sobremesas/sagu-com-vinho-tinto-4d544b2d52e0b20f76001631.ghtml",
+            "Quentão (3 lts)": "https://receitas.globo.com/receitas-da-tv/mais-voce/quentao-4d50796652e0b252bc00176a.ghtml",
+            "Vinho quente (20 unidades)": "https://receitas.globo.com/datas-comemorativas/festa-junina/vinho-quente-5583143c4d38853625000045.ghtml",
         }
     }
 };
 
-function calculate(guests) {
-    const items = festaData[guests];
+function normalizeString(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().replace(/ /g, '-').replace(/[\(\),]/g, '');
+}
+
+function handleInput() {
+    const guests = parseInt(document.getElementById('guestInput').value);
+    let category;
+
+    if (isNaN(guests) || guests <= 0) {
+        alert("Por favor, insira um número válido de participantes.");
+        return;
+    }
+
+    if (guests <= 10) {
+        category = 10;
+    } else if (guests <= 20) {
+        category = 20;
+    } else if (guests <= 30) {
+        category = 30;
+    } else {
+        category = 40;
+    }
+
+    calculate(category, guests);
+}
+
+function calculate(category, guests) {
+    const items = festaData[category];
+    const responseElement = document.getElementById('response');
+    responseElement.innerHTML = `<h3 class="suggestion-title">Para ${guests} pessoas, sugerimos no mínimo os seguintes itens:</h3>`;
+    responseElement.classList.remove('hidden');
+
     let resultsHTML = "";
+
+    // Lista de itens que não devem ter links de receitas
+    const noLinkItems = ["Latas refrigerante", "Lata cerveja (335ml)", "Latas suco", "Latas água"];
     
     Object.keys(items).forEach(key => {
         if (key !== "recipeLink") {
             const quantity = items[key];
             const recipeLink = items.recipeLink[key];
             if (quantity !== null) {
+                const normalizedKey = normalizeString(key);
                 resultsHTML += `<div class="item">
-                                    <img src="${key.toLowerCase().replace(/ /g, '-')}.jpg" alt="${key}">
-                                    <p>${key}: ${quantity} <a href="${recipeLink}" target="_blank">Receita</a></p>
-                                </div>`;
+                                    <img src="images/${normalizedKey}.jpg" alt="${key}">
+                                    <p>${key}: ${quantity}</p>`;
+                // Adiciona o link apenas se o item não estiver na lista noLinkItems
+                if (!noLinkItems.includes(key) && recipeLink) {
+                    resultsHTML += `<p><button onclick="window.open('${recipeLink}', '_blank')">Receita</button></p>`;
+                }
+                resultsHTML += `</div>`;
             }
         }
     });
 
-    document.getElementById('results').innerHTML = resultsHTML;
+    // Exibir os resultados
+    const resultsElement = document.getElementById('results');
+    resultsElement.innerHTML = resultsHTML;
+    resultsElement.classList.remove('hidden');
 }
